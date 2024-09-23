5a6eab5c686ddae9dc918e75afd7f987a2e10d60
Felipe <felipermpires@gmail.com>
2024-09-22 21:50:31 -0300

    inicia menu de aplicação

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
-function start() {
-    let count = 1
-    while(count <= 10) {
-        console.log(count)
-        count++
-    }
+const { select } = require("@inquirer/prompts")
 
+// 22:14
+const start = async () => {
+  while (true) {
+    const opcao = await select({
+      message: "menu >",
+      choices: [
+        {
+          name: "cadastrar metas",
+          value: "cadastrar",
+        },
+        {
+          name: "listar metas",
+          value: "listar",
+        },
+        {
+          name: "sair",
+          value: "sair",
+        },
+      ],
+    })
+
+    switch (opcao) {
+      case "cadastrar":
+        console.log("vamos cadastrar")
+        break
+      case "listar":
+        console.log("vamos listar")
+        break
+      case "sair":
+        console.log("Até a proxima!")
+        return
+    }
+  }
 }
 
 start()
